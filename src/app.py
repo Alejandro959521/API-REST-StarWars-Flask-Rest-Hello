e209f84a2098cb865a8b0bd7f3d882073ce0837e
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2023-09-10 16:25:39 +0000

    agregado de las rutas delete de favoritos

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,Usuario,Personajes,Planetas,Favoritos
 #from models import Person
 
 app = Flask(__name__)
@@ -43,9 +43,319 @@ def handle_hello():
         "msg": "Hello, this is your GET /user response "
     }
 
-    return jsonify(response_body), 200
+    return jsonify(response_body),   200
 
 # this only runs if `$ python src/app.py` is executed
+
+@app.route("/usuario", methods=["POST"])
+def create_user():
+    body = request.json
+    name = body.get("name")
+    email = body.get("email")
+    password = body.get("password")
+    
+    if email is None or password is None or name is None:
+        return jsonify({
+            "message": "Email, password and name are required"
+        }), 400
+    
+    user_exist = Usuario.query.filter_by(email=email).one_or_none()
+    if user_exist is not None:
+        return jsonify({
+            "message": "User already exists"
+        }), 400
+    user = Usuario(email, password, name)
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "ocurrió un error interno",
+            "error": error.args
+        }), 500
+    return jsonify({}), 201
+
+@app.route("/usuario", methods=["GET"])
+def get_all_user():
+    
+    user = Usuario.query.all()
+    print("esto es user",user)
+    if user is None:
+
+         return jsonify(""), 404
+    usuario=[fav.serialize() for fav in user]
+
+    return jsonify(usuario), 200
+
+@app.route("/planetas", methods=["GET"])
+def get_all_planets():
+    
+    valor = Planetas.query.all()
+   
+    if valor is None:
+
+         return jsonify(""), 404
+    planetas=[fav.serialize() for fav in valor]
+
+    return jsonify(planetas), 200
+
+@app.route("/personajes", methods=["GET"])
+def get_all_personajes():
+    
+    valor = Personajes.query.all()
+    
+    if valor is None:
+
+         return jsonify(""), 404
+    planetas=[fav.serialize() for fav in valor]
+
+    return jsonify(planetas), 200
+
+@app.route("/usuario/<int:id>", methods=["GET"])
+def get_user(id):
+    if id is None: 
+        return jsonify({
+            "message": "id is required"
+        }), 400
+    user = Usuario.query.get(id)
+    if user is None:
+
+         return jsonify(""), 404
+    return jsonify(user.serialize()), 200
+
+
+@app.route("/personajes", methods=["POST"])
+def create_personaje():
+    body = request.json
+    name = body.get("name")
+    person_class = body.get("person_class")
+    faccion = body.get("faccion")
+    race = body.get("race")
+    gender = body.get("gender")
+    
+    if person_class is None or faccion is None or race is None or gender is None or name is None:
+        return jsonify({
+            "message": "All information are required"
+        }), 400
+    
+    personajes = Personajes(
+    faccion=faccion,
+    person_class=person_class,
+    race=race,
+    gender=gender,
+    name=name
+    )
+
+    try:
+        db.session.add(personajes)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "ocurrió un error interno",
+            "error": error.args
+        }), 500
+    return jsonify({}), 201
+
+
+@app.route("/personajes/<int:id>", methods=["GET"])
+def get_personaje(id):
+    if id is None: 
+        return jsonify({
+            "message": "id is required"
+        }), 400
+    personaje = Personajes.query.get(id)
+    if personaje is None:
+         return jsonify(""), 404
+    return jsonify(personaje.serialize()), 200
+
+
+@app.route("/planetas", methods=["POST"])
+def create_planeta():
+    body = request.json
+    name = body.get("name")
+    size = body.get("size")
+    temp = body.get("temp")
+    color = body.get("color")
+    moon_numbers = body.get("moon_numbers")
+    
+    if name is None or size is None or temp is None or color is None or moon_numbers is None:
+        return jsonify({
+            "message": "All information are required"
+        }), 400
+    
+    planetas = Planetas(
+    moon_numbers=moon_numbers,
+    size=size,
+    temp=temp,
+    color=color,
+    name= name
+    )
+
+    try:
+        db.session.add(planetas)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "ocurrió un error interno",
+            "error": error.args
+        }), 500
+    return jsonify({}), 201
+
+@app.route("/planetas/<int:id>", methods=["GET"])
+def get_planetas(id):
+    if id is None: 
+        return jsonify({
+            "message": "id is required"
+        }), 400
+    planetas = Planetas.query.get(id)
+    if  planetas is None:
+         return jsonify(""), 404
+    return jsonify( planetas.serialize()), 200
+
+@app.route("/usuario/<int:id>/favoritos", methods=["GET"])
+def get_favoritos(id):
+    if id is None: 
+        return jsonify({
+            "message": "id is required"
+        }), 400
+    favorito = Usuario.query.get(id)
+    if favorito is None:
+         return jsonify(""), 404
+    return jsonify({"favoritos":favorito.serialize()["favoritos"]}), 200
+         
+@app.route("/usuario/<int:user_id>/favoritos/planetas/<int:planet_id>", methods=["POST"])
+def agregar_planeta(user_id,planet_id):
+    
+    body = request.json
+    planeta=Planetas.query.get(planet_id)
+    usuario=Usuario.query.get(user_id)
+
+    
+    if planeta is None or usuario is None:
+        return jsonify({
+            "message": "All information are required"
+        }), 400
+    
+    favoritos = Favoritos(
+        user_id=user_id,
+        planet_id=planet_id
+    )
+
+    try:
+        db.session.add(favoritos)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "ocurrió un error interno",
+            "error": error.args
+        }), 500
+    return jsonify({}), 201
+
+
+@app.route("/usuario/<int:user_id>/favoritos/personajes/<int:personaje_id>", methods=["POST"])
+def agregar_personaje(user_id,personaje_id):
+    
+    body = request.json
+    personaje=Personajes.query.get(personaje_id)
+    usuario=Usuario.query.get(user_id)
+
+    
+    if personaje is None or usuario is None:
+        return jsonify({
+            "message": "All information are required"
+        }), 400
+    
+    favoritos = Favoritos(
+        user_id=user_id,
+        personaje_id=personaje_id
+    )
+
+    try:
+        db.session.add(favoritos)
+        db.session.commit()
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({
+            "message": "ocurrió un error interno",
+            "error": error.args
+        }), 500
+    return jsonify({}), 201
+
+
+
+
+@app.route("/usuario/<int:id>", methods=["DELETE"])
+def delete_purchase_order(id):
+    if id is None: 
+        return jsonify({
+            "message": "id is required"
+        }), 400
+    usuario = Usuario.query.get(id)
+    print("wwwwwwwwwwwwwwwwwwwwww",usuario)
+    if usuario is None:
+        return jsonify(None), 404
+    # Elimina la orden de la db. Revisa app.py el método delete_and_commit
+    deleted = usuario.delete_and_commit()
+    # Si la variable delted es False, quiere decir que no se guardó y ocurrió un error
+    if not deleted:
+        return jsonify({
+            "message": "Something happened with db, try again"
+        }), 500
+    # Retorna status code indicando que todo salió bien, pero sin contenido
+    # Revisar: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE#responses
+    return jsonify(None), 204
+
+@app.route("/usuario/<int:user_id>/favoritos/planetas/<int:planet_id>", methods=["DELETE"])
+def delete_planet(planet_id,user_id):
+    if planet_id is None or user_id is None: 
+        return jsonify({
+            "message": "id is required"
+        }), 400
+    planet = Favoritos.query.filter_by(planet_id=planet_id, user_id=user_id).first()
+    print("ffffffffffffffffffffffffffesto es planet",planet)
+    if planet is None:
+        return jsonify(None), 404
+    # Elimina la orden de la db. Revisa app.py el método delete_and_commit
+    deleted = Favoritos.delete(planet)
+    # Si la variable delted es False, quiere decir que no se guardó y ocurrió un error
+    if not deleted:
+        return jsonify({
+            "message": "Something happened with db, try again"
+        }), 500
+    # Retorna status code indicando que todo salió bien, pero sin contenido
+    # Revisar: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE#responses
+    return jsonify(None), 204
+
+
+@app.route("/usuario/<int:user_id>/favoritos/personajes/<int:personaje_id>", methods=["DELETE"])
+def delete_personaje(personaje_id,user_id):
+    if personaje_id is None or user_id is None: 
+        return jsonify({
+            "message": "id is required"
+        }), 400
+    personaje = Favoritos.query.filter_by(personaje_id=personaje_id, user_id=user_id).first()
+    
+    if personaje is None:
+        return jsonify(None), 404
+    # Elimina la orden de la db. Revisa app.py el método delete_and_commit
+    deleted = Favoritos.delete(personaje)
+    # Si la variable delted es False, quiere decir que no se guardó y ocurrió un error
+    if not deleted:
+        return jsonify({
+            "message": "Something happened with db, try again"
+        }), 500
+    # Retorna status code indicando que todo salió bien, pero sin contenido
+    # Revisar: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE#responses
+    return jsonify(None), 204
+
+
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -15,5 +15,166 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+           
         }
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True )
+    name = db.Column(db.String(250), nullable=False,unique=False)  
+    email = db.Column(db.String(250),nullable=False,unique=True)
+    password = db.Column(db.String(450), nullable=False,unique=False)
+    favoritos = db.relationship("Favoritos",back_populates="usuario")
+
+    def __init__(self, email, password, name):
+        self.email = email
+        self.password = password
+        self.name = name
+
+    def __repr__(self):
+        return f'<Usuarios {self.email} {self.password} {self.name}>'
+
+    def serialize(self):
+        favoritos=[fav.serialize() for fav in self.favoritos]
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "favoritos":favoritos
+           
+        }
+  
+    def delete_and_commit(self):
+        """
+            Elimina la instancia a la sesión (delete) y salva los cambios en la base de datos (commit).
+            Si algo sale mal en este proceso, lo deshace (rollback) y retorna False.
+            Si todo sale bien, retorna True
+        """
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return True
+        except Exception as error:
+            print("Error del tipo", type(error))
+            print("Error: ", error)
+            print("Error args: ", error.args)
+            db.session.rollback()
+            return False
+
+class Personajes(db.Model):
+    __tablename__ = 'personajes'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=True)
+    person_class = db.Column(db.String(250), nullable=True)
+    faccion = db.Column(db.String(250), nullable=True)
+    race = db.Column(db.String(250), nullable=True)
+    gender =db.Column(db.String(250), nullable=True)
+    favoritos = db.relationship("Favoritos",back_populates="personajes")
+    
+
+    def __init__(self,**kwargs):
+        self.name =  kwargs["name"]
+        self.faccion = kwargs["faccion"]
+        self.person_class=  kwargs["person_class"]
+        self.race =  kwargs["race"]
+        self.gender =  kwargs["gender"]
+
+    def __repr__(self):
+        return f'<personajes {self.faccion} {self.person_class} {self.name} {self.race} {self.gender}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "faccion": self.faccion,
+            "person_class": self.person_class,
+            "race":self.race,
+            "gender":self.gender,
+            "name":self.name
+
+            
+        }
+
+  
+class Planetas(db.Model):    
+    __tablename__ = 'planetas'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable= True)
+    size = db.Column(db.String(250), nullable= True)
+    temp =  db.Column(db.Integer, nullable= True)
+    color = db.Column(db.String(250), nullable= True)
+    moon_numbers = db.Column(db.String(250), nullable= True)
+    favoritos=db.relationship("Favoritos",back_populates="planetas")
+
+    
+    def __init__(self,**kwargs):
+        self.name =  kwargs["name"]
+        self.size = kwargs["size"]
+        self.temp =  kwargs["temp"]
+        self.color =  kwargs["color"]
+        self.moon_numbers =  kwargs["moon_numbers"]
+
+    def __repr__(self):
+        return f'<planetas {self.size} {self.temp} {self.color} {self.moon_numbers} {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "size": self.size,
+            "temp": self.temp,
+            "color":self.color,
+            "moon_numbers":self.moon_numbers,
+            "name":self.name   
+
+            
+        }
+
+        
+
+
+class Favoritos(db.Model):
+    __tablename__ = 'favoritos'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.ForeignKey('usuario.id'), nullable=True)
+    usuario=db.relationship("Usuario",back_populates="favoritos")  
+    personaje_id = db.Column(db.ForeignKey('personajes.id'), nullable=True)
+    personajes=db.relationship("Personajes",back_populates="favoritos") 
+    planet_id = db.Column(db.ForeignKey('planetas.id'), nullable=True)
+    planetas=db.relationship("Planetas",back_populates="favoritos") 
+
+    def serialize(self) :
+      
+        return {
+
+            "id": self.id,
+            "user_id":self.user_id,
+            "personaje_id":self.personaje_id,       
+            "planet_id": self.planet_id,
+            
+
+            #"planeta": self.planetas.serialize(),
+
+            #"personaje": self.personajes.serialize()
+        }
+
+    # def serializeplaneta(self):
+    #     return {
+
+    #     "planeta": self.planetas.serialize()
+    #     }
+    def delete(self):
+        """
+            Elimina la instancia a la sesión (delete) y salva los cambios en la base de datos (commit).
+            Si algo sale mal en este proceso, lo deshace (rollback) y retorna False.
+            Si todo sale bien, retorna True
+        """
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return True
+        except Exception as error:
+            print("Error del tipo", type(error))
+            print("Error: ", error)
+            print("Error args: ", error.args)
+            db.session.rollback()
+            return False        
